d61fc2d1cc5be9d2363d31d00eb3396c2acd899e
tonygiang <tony_s_giang@yahoo.com>
2022-07-27 15:59:24 +0700

    Project push.

---
+++ IList.SwapIndices.cs
@@ -0,0 +1,57 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+using System.Collections.Generic;
+
+namespace CLSS
+{
+  public static partial class IListSwapIndices
+  {
+    /// <summary>
+    /// Swaps 2 elements at the specified index positions in place.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IList{T}"/> to swap indices.
+    /// </typeparam>
+    /// <typeparam name="TElement">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IList{T}"/> to swap indices.</param>
+    /// <param name="index1">The first index number to be swapped.</param>
+    /// <param name="index2">The second index number to be swapped.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is
+    /// null.</exception>
+    public static T SwapIndices<T, TElement>(this T source,
+      int index1,
+      int index2)
+      where T : IList<TElement>
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      var e1 = source[index1];
+      source[index1] = source[index2];
+      source[index2] = e1;
+      return source;
+    }
+
+    /// <summary>
+    /// Swaps 2 elements at the specified index positions in place.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="source">The <see cref="IList{T}"/> to swap indices.</param>
+    /// <param name="index1">The first index number to be swapped.</param>
+    /// <param name="index2">The second index number to be swapped.</param>
+    /// <returns>The source collection.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> is
+    /// null.</exception>
+    public static IList<T> SwapIndices<T>(this IList<T> source,
+      int index1,
+      int index2)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      var e1 = source[index1];
+      source[index1] = source[index2];
+      source[index2] = e1;
+      return source;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+﻿# CLSS.ExtensionMethods.IList.SwapIndices
+
+### Problem
+
+Swapping indices in a list is a common use case is a common use case that usually requires 3 lines of code:
+
+```
+var tempVal = collection[1];
+collection[1] = collection[4];
+collection[4] = tempVal;
+```
+
+### Solution
+
+`SwapIndices` in an extension method that reduces this common operation to 1 line of code:
+
+```
+using CLSS;
+
+collection.SwapIndices(1, 4);
+```
+
+`SwapIndices` returns the source `Ilist<T>` to be friendly to a functional-style call chain. The return type will be determined by the invocation syntax of `SwapIndices`. With an implicit type invocation, it returns an `IList<T>`. With an explicit type invocation, it returns the original collection type.
+
+```
+using CLSS;
+
+var numbers = new int[] { 0, 1, 2, 3, 4 };
+numbers.SwapIndices(1, 4); // returns IList<int>
+numbers.SwapIndices<int[], int>(1, 4); // returns int[];
+```
+
+**Note**: `SwapIndices` works on all types implementing the [`IList<T>`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.ilist-1) interface, *including raw C# array*.
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
